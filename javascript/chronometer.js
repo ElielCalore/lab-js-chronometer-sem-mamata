221fab9c2fb2c2b89fba5bb9ee9d4f2df7178ca5
eliel <elieldscalore@gmail.com>
2022-06-07 17:53:21 -0300

    07062022

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,39 +1,47 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
-
-  start(printTimeCallback) {
-    // ... your code goes here
-  }
-
-  getMinutes() {
-    // ... your code goes here
-  }
-
-  getSeconds() {
-    // ... your code goes here
-  }
-
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
-
-  stop() {
-    // ... your code goes here
-  }
-
-  reset() {
-    // ... your code goes here
-  }
-
-  split() {
-    // ... your code goes here
-  }
+    constructor() {
+        this.currentTime = 0;
+        this.intervalId = null;
+    }
+    start(printTimeCallback) {
+        this.intervalId = setInterval(() => {
+            this.currentTime += 1;
+            if (printTimeCallback) {
+                printTimeCallback();
+            }
+        }, 1000);
+    }
+    getMinutes() {
+        return Math.floor(this.currentTime / 60);
+    }
+    getSeconds() {
+        return Math.floor(this.currentTime % 60);
+    }
+    computeTwoDigitNumber(value) {
+        let aux = value.toString();
+        if (aux.length === 1) {
+            return '0' + aux;
+        } else {
+            return aux;
+        }
+    }
+    stop() {
+        clearInterval(this.intervalId);
+    }
+    reset() {
+        this.currentTime = 0;
+    }
+    split() {
+        return (
+            this.computeTwoDigitNumber(this.getMinutes()) +
+            ':' +
+            this.computeTwoDigitNumber(this.getSeconds())
+        );
+    }
 }
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
-  module.exports = Chronometer;
+    module.exports = Chronometer;
 }
